5a8dc3959fa449ba6835004e6e4f5fb0d070ce53
Emrullah Aydın <emrullahaydin@Emrullah-MacBook-Pro.local>
2026-03-27 16:57:26 +0300

    fix: dynamically generate node path for xcode cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
-# Neden: Xcode Cloud sunucularında Node.js ve bağımlılıkları kurarak .xcworkspace dosyasını dinamik olarak üretiriz.
-
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 
-# Gerekli ortam araçlarını kur
 brew install node
 brew install cocoapods
 
-# Betik ios/ci_scripts içinde çalıştığı için, proje ana dizinine dön (2 üst klasör)
 cd ../../
-
-# JavaScript bağımlılıklarını kur (Eğer yarn kullanıyorsanız 'npm install' yerine 'yarn install' yazın)
 npm install
 
-# iOS klasörüne dön ve pod'ları kur
 cd ios
+# KİLİT NOKTA: Apple sunucusunun kendi kurduğu Node.js yolunu bulup,
+# derleme esnasında kaybolmaması için sadece sunucuda geçerli bir adres dosyası üretiyoruz.
+echo "export NODE_BINARY=$(command -v node)" > .xcode.env.local
+
 pod install
